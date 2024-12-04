0c5777f0a887f557d9900ea6774c0bad37ced089
SERGEYDJUM <39106181+SERGEYDJUM@users.noreply.github.com>
2024-12-04 17:32:52 +0300

    Add created_at back

--- cobwebai/models/user.py
+++ cobwebai/models/user.py
@@ -1,20 +1,16 @@
 # Модель для хранения информации о пользователе
-from sqlalchemy import Column, String, DateTime
+from sqlalchemy import Column, DateTime
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 from cobwebai.models.base import Base
-from datetime import datetime, timezone
 from fastapi_users.db import SQLAlchemyBaseUserTableUUID
 
 
 class User(SQLAlchemyBaseUserTableUUID, Base):
     __tablename__ = "users"
 
-    # # Имя пользователя
-    # username = Column(String, nullable=False, unique=True)
-    # # Хешированный пароль пользователя
-    # password = Column(String, nullable=False)
-    # # Дата регистрации пользователя
-    # created_at = Column(DateTime, default=datetime.now(tz=timezone.utc))
+    # Дата регистрации пользователя
+    created_at = Column(DateTime(timezone=False), server_default=func.now())
 
     # Связь с проектами
     projects = relationship("Project", back_populates="user")
