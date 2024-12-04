75ac994dffd71c58e606e635ace42437aaaae101
SERGEYDJUM <39106181+SERGEYDJUM@users.noreply.github.com>
2024-12-04 21:26:49 +0300

    Make fix-transcription use json

--- cobwebai/api/llm.py
+++ cobwebai/api/llm.py
@@ -1,6 +1,7 @@
 from cobwebai_lib.audio import Transcription
 from cobwebai_lib.text import TextPostProcessing
 from fastapi import APIRouter, Depends, UploadFile, HTTPException
+from pydantic import BaseModel
 from aiofiles import tempfile
 from aiofiles import open as aio_open
 from openai import AsyncOpenAI
@@ -15,6 +16,13 @@ from cobwebai.settings import settings
 SIZE_100MB = 1024 * 1024 * 100
 SIZE_1GB = 1024 * 1024 * 1024
 
+
+class FixTranscriptionInput(BaseModel):
+    text: str
+    theme: str | None = None
+    chunk_size: int | None = None
+
+
 openai_client = AsyncOpenAI(api_key=settings.openapi_key)
 transcription_provider = Transcription(oai_client=openai_client, log=logger)
 postprocessing_provider = TextPostProcessing(oai_client=openai_client, log=logger)
@@ -27,7 +35,7 @@ async def transcribe_file(
     avfile: UploadFile, language: str = "ru", user: User = Depends(current_active_user)
 ):
     """Transcribes an audio/video file into text"""
-    
+
     if language not in ["ru", "en"]:
         raise HTTPException(400, "Unsupported language")
 
@@ -48,4 +56,21 @@ async def transcribe_file(
         if text := await transcription_provider.transcribe_file(named_file_path):
             return text
         else:
-            raise HTTPException(422, "Failed to process the file")
+            raise HTTPException(500, "Failed to process the file")
+
+
+@llm_router.post("/fix-transcription")
+async def transcribe_file(
+    json_input: FixTranscriptionInput,
+    user: User = Depends(current_active_user),
+):
+    """Fixes transcribed text via GPT-4o-mini"""
+
+    if text := await postprocessing_provider.fix_transcribed_text(
+        json_input.text,
+        json_input.theme,
+        json_input.chunk_size if json_input.chunk_size > 0 else None,
+    ):
+        return text
+    else:
+        raise HTTPException(500, "Failed to process the text")
